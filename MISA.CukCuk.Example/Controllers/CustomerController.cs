ca0014d0523c2dc077eec7845154483b109b034c
DUNG TA DINH <tddung@rd.local>
2021-04-17 11:59:20 +0700

    get and pust

---
+++ MISA.CukCuk.Example/Controllers/CustomerController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySqlConnector;
+using Dapper;
+using System.Data;
+using MISA.CukCuk.Example.Model;
+
+
+namespace MISA.CukCuk.Example.Controllers
+{
+    [Route("api/v1/[controller]s")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            String connectionString = "" +
+                "Host = 47.241.69.179;" +
+                "Port = 3306;" +
+                "Database = MF0_NVManh_CukCuk02;" +
+                "User Id = dev;" +
+                "Password = 12345678;";
+
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var customers = dbConnection.Query<Customer>("Proc_GetCustomers", commandType: CommandType.StoredProcedure);
+
+            if (customers.Count() > 0)
+            {
+                return Ok(customers);
+            }
+            else return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Customer customer)
+        {
+
+            String connectionString = "" +
+            "Host = 47.241.69.179;" +
+            "Port = 3306;" +
+            "Database = MF0_NVManh_CukCuk02;" +
+            "User Id = dev;" +
+            "Password = 12345678;";
+
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+
+            DynamicParameters dynamicParameters = new DynamicParameters();
+
+            dynamicParameters.Add("@m_CustomerCode", customer.CustomerCode);
+
+            var customersCodeExists = dbConnection.QueryFirstOrDefault<bool>("Proc_CheckCustomerCodeExists", dynamicParameters, commandType: CommandType.StoredProcedure);
+            if (customersCodeExists == true)
+            {
+                var response = new
+                {
+                    devMsg = "Mã khách hàng đã tồn tại trong hệ thống.",
+                    MISACode = "002",
+                };
+                return BadRequest(response);
+            }
+
+            var rowsAffect = dbConnection.Execute("Proc_InsertCustomer", param: customer, commandType: CommandType.StoredProcedure);
+            if (rowsAffect > 0)
+            {
+                return StatusCode(201, rowsAffect);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ MISA.CukCuk.Example/Model/Customer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Example.Model
+{
+    public class Customer
+    {
+        ///<summary>
+        ///Khoá chính
+        /// </summary>
+        public Guid CustomerId { get; set; }
+
+        ///<summary>
+        ///Mã khsach hàng
+        /// </summary>
+        public string CustomerCode { get; set; }
+
+        ///<summary>
+        ///Họ và tên
+        /// </summary>
+        public string Fullname { get; set; }
+
+        ///<summary>
+        //Giới tính
+        /// </summary>
+        public int? Gender { get; set; }
+
+        ///<summary>
+        ///Mã thẻ
+        /// </summary>
+        public string MemberCardCode { get; set; }
+
+        ///<summary>
+        ///Nhóm khách hàng
+        /// </summary>
+        public Guid? CustomerGroupId { get; set; }
+
+        ///<summary>
+        ///Số điện thoại
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        ///<summary>
+        ///Ngày sinh
+        /// </summary>
+        public DateTime DateOfBirth { get; set;}
+
+        ///<summary>
+        ///Tên công ty
+        /// </summary>
+        public string CompanyName { get; set; }
+
+        ///<summary>
+        ///Mã tax công ty
+        /// </summary>
+        public string CompanyTaxCode { get; set; }
+
+        ///<summary>
+        ///Email
+        /// </summary>
+        public string Email { get; set; }
+
+        ///<summary>
+        ///Địa chỉ
+        /// </summary>
+        public string Address { get; set; }
+
+        ///<summary>
+        ///Ghi chú
+        /// </summary>
+        public string Note { get; set; }
+
+        ///<summary>
+        ///Ngày tạo
+        /// </summary>
+        public DateTime? CreatedDate { get; set; }
+
+        ///<summary>
+        ///Tạo bởi
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        ///<summary>
+        ///Ngày sửa đổi
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+
+        ///<summary>
+        ///Sửa bởi
+        /// </summary>
+        public string ModifiedBy { get; set; }
+
+    }
+}
